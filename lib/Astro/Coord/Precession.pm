c695835016491754d9008a0f32d24a0282ad27be
Dimitrios Kechagias <dimitrios.kechagias@spareroom.co.uk>
2021-04-04 23:08:49 +0100

    v0.02 - README

--- lib/Astro/Coord/Precession.pm
+++ lib/Astro/Coord/Precession.pm
@@ -15,11 +15,11 @@ Astro::Coord::Precession - Precess coordinates between 2 epochs
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Exporter qw(import);
 
